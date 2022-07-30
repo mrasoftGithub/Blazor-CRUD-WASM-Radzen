256697cddd6284ca2f27d16a83faa07000c70242
MRAsoft <info.mrasoft@gmail.com>
2022-07-30 20:14:24 +0200

    Add project files.

---
+++ Shared/Fout.cs
@@ -0,0 +1,13 @@
+﻿namespace RadzenMRA.Shared
+{
+    public class Fout
+    {
+        public string? Type { get; set; } = string.Empty;
+        public string? Title { get; set; } = string.Empty;
+        public int StatusCode { get; set; }
+        public string? Detail { get; set; } = string.Empty;
+        public string? Instance { get; set; } = string.Empty;
+        public string? TraceId { get; set; } = string.Empty;
+
+    }
+}
